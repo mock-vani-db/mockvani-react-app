18b0d65bd9f22a44b03b4f77d33d286ab3eaa794
mock-vani-db <dbhupati@gmail.com>
2024-11-29 09:40:55 -0800

    Remove unnecessary folders and files

--- mock-vani-ai/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://mockvani_owner:k8Zv1tphnAFY@ep-noisy-violet-a5n0ns6p.us-east-2.aws.neon.tech/mockvani?sslmode=require',
-    }
-  };
--- mock-vani-ai/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['unsplash.com', 'images.unsplash.com'], // Add unsplash.com to the list of allowed image domains
-    },
-  };
-  
-  export default nextConfig;
-  
